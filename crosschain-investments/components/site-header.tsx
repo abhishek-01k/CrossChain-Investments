0a608247b6de8d00be572bd11055a0379902ae4b
abhishek-01k <abhishek@push.org>
2024-10-13 01:00:04 +0530

    Added Circle Crypto Deposit API

--- crosschain-investments/components/site-header.tsx
+++ crosschain-investments/components/site-header.tsx
@@ -26,11 +26,10 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            
             <ConnectButton
               client={client}
               wallets={wallets}
-              chains={[sepolia, polygonAmoy]}
+              chains={[polygonAmoy]}
             />
 
             <ThemeToggle />
